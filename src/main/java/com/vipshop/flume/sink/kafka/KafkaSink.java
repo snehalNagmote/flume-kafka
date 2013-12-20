7869f4ee2b0de7968e1e3de58ba7edce6563147f
Snehal Nagmote <snagmote@kixeye.com>
2013-12-20 15:03:54 -0800

    Updated Flume-kafka Sink to work with Kafka-0.8

--- src/main/java/com/vipshop/flume/sink/kafka/KafkaSink.java
+++ src/main/java/com/vipshop/flume/sink/kafka/KafkaSink.java
@@ -1,7 +1,23 @@
+/*******************************************************************************
+ * Copyright 2013 Renjie Yao
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
 package com.vipshop.flume.sink.kafka;
 
 import kafka.javaapi.producer.Producer;
-import kafka.javaapi.producer.ProducerData;
+
+import kafka.producer.KeyedMessage;
 
 import org.apache.flume.Channel;
 import org.apache.flume.Context;
@@ -16,6 +32,7 @@ import org.slf4j.LoggerFactory;
 import com.vipshop.flume.KafkaUtil;
 
 public class KafkaSink extends AbstractSink implements Configurable{
+	
 	private static final Logger log = LoggerFactory.getLogger(KafkaSink.class);
 	private String topic;
 	private Producer<String, String> producer;
@@ -26,15 +43,18 @@ public class KafkaSink extends AbstractSink implements Configurable{
 		try {
 			tx.begin();
 			Event e = channel.take();
-			if(e==null) {
+			if(e == null) {
 				tx.rollback();
 				return Status.BACKOFF;
 			}
-			producer.send(new ProducerData<String, String>(this.topic, new String(e.getBody())));
-			log.trace("Message: " + e.getBody());
+			KeyedMessage< String, String> keyMessageData=new KeyedMessage<String, String>(topic,new String(e.getBody()));
+			//producer.send(new ProducerData<String, String>(topic, new String(e.getBody())));
+			producer.send(keyMessageData);
+			log.trace("Message: {}", e.getBody());
 			tx.commit();
 			return Status.READY;
 		} catch(Exception e) {
+			log.error("KafkaSink Exception:{}",e);
 			tx.rollback();
 			return Status.BACKOFF;
 		} finally {
@@ -43,8 +63,8 @@ public class KafkaSink extends AbstractSink implements Configurable{
 	}
 
 	public void configure(Context context) {
-		this.topic = KafkaUtil.getKafkaConfigParameter(context, "topic");
-		this.producer = KafkaUtil.getProducer(context);
+		topic = context.getString("topic");
+		producer = KafkaUtil.getProducer(context);
 	}
 
 	@Override
